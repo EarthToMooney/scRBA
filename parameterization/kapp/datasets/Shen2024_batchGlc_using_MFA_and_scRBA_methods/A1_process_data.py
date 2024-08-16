e8922508dc8c93b4c76b26ad45d8f6710631a579
Eric Mooney <ejm6426@psu.edu>
2024-08-15 21:42:29 -0400

    a1

--- parameterization/kapp/datasets/Shen2024_batchGlc_using_MFA_and_scRBA_methods/A1_process_data.py
+++ parameterization/kapp/datasets/Shen2024_batchGlc_using_MFA_and_scRBA_methods/A1_process_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import requests
 import json
+import os
 
 import sys
 sys.path.append('../../../../pycore/')
@@ -25,9 +26,11 @@ ptot = 0.580680092
 # Mass fraction of proteome that's not explicitly modeled. Set to 0 if recalculate_nonmodeled_proteome_allocation = True.
 nonmodeled_proteome_allocation = 0
 nonmodel_protein_data_path = './nonmodeled_proteins.json'
-with open(nonmodel_protein_data_path, 'w') as f:
-    # load from file
-    nonmodel_proteins = json.load(f)
+# if path exists
+if os.path.exists(nonmodel_protein_data_path): 
+    with open(nonmodel_protein_data_path, 'r') as f:
+        # load from file
+        nonmodel_proteins = json.load(f)
 recalculate_nonmodeled_proteome_allocation = True
 # Max mass fraction of modeled proteome that's inside the mitochondria. Set to 0 by default and automatically adjusted if recalculate_mito_proteome_allocation = True.
 max_allowed_mito_proteome_allo_fraction = 0
@@ -118,47 +121,48 @@ if recalculate_nonmodeled_proteome_allocation:
         if i not in df_prot.index:
             # print(i, "not in df_prot")
             # add to dummy protein and nonmodeled proteome allocation calculations
-            nonmodeled_proteome_allocation += df_raw.loc[i, cols_data]
+            nonmodeled_proteome_allocation += df_raw.loc[i, cols_data].sum()
             # check nonmodeled_proteins for sequence, MW, and conc
-            if uniprot_col == '':
-                seq = ''
-                conc = 0
-                mw = 0
-            else:
-                for p in nonmodel_proteins:
-                    if p['id'] == i:
-                        seq = p['sequence']
-                        conc = p['conc (g/gDW)']
-                        mw = p['MW (g/mmol)']
-                        break
-            # consider scraping uniprot for sequence or using API if they have one
-            if search_uniprot_for_nonmodeled_sequences and seq == '':
-                # search uniprot for protein sequence
-                url = uniprot_url + df_raw.loc[i, uniprot_col] + '?format=json'
-                # get response, convert to dict
-                # response = requests.get(url).json()
-                response = requests.get(url,headers=headers).json()
-                # print('response:',response)
-                # find "sequence" key
-                if 'sequence' in response:
-                    # add sequence to dummy protein
-                    seq = response['sequence']['value']
-                    # convert this excel formula into a method of determining protein mass: =SUMPRODUCT((LEN([@sequence])-LEN(SUBSTITUTE([@sequence],{"A";"C";"D";"E";"F";"G";"H";"I";"K";"L";"M";"N";"P";"Q";"R";"S";"T";"V";"W";"Y"},""))),{72.08;104.14;115.08;129.11;148.17;58.05;138.14;114.16;130.18;114.16;132.2;115.1;98.12;129.13;158.19;88.08;102.1;100.13;187.21;164.17})/1000
-                    mw = sum([len(seq) - len(seq.replace(aa, '')) for aa in ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']] * np.array([72.08,104.14,115.08,129.11,148.17,58.05,138.14,114.16,130.18,114.16,132.2,115.1,98.12,129.13,158.19,88.08,102.1,100.13,187.21,164.17])) / 1000
-                    # abundance / wt.
-                    conc = df_raw.loc[i, cols_data[0]]
-                    for aa in ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']:
-                        # find fraction of amino acid in sequence, multiply by abundance / MW of protein
-                        dummy_protein['AA abundances'][aa] += (len(seq) - len(seq.replace(aa, '')) / len(seq)) * conc / mw
-                    # with open('./nonmodeled_proteins.json', 'a') as f:
-                    #     f.write(str({'id':i,'URL':url,'sequence':seq,'MW (g/mmol)':mw,'conc (g/gDW)':conc}))
-                    nonmodel_proteins.append({'id':i,'URL':url,'sequence':seq,'MW (g/mmol)':mw,'conc (g/gDW)':conc})
+        #     if uniprot_col == '':
+        #         seq = ''
+        #         conc = 0
+        #         mw = 0
+        #     else:
+        #         for p in nonmodel_proteins:
+        #             if p['id'] == i:
+        #                 seq = p['sequence'].replace('*','')
+        #                 conc = p['conc (g/gDW)']
+        #                 mw = p['MW (g/mmol)']
+        #                 break
+        #     # consider scraping uniprot for sequence or using API if they have one
+        #     if search_uniprot_for_nonmodeled_sequences and not seq:
+        #         # search uniprot for protein sequence
+        #         url = uniprot_url + df_raw.loc[i, uniprot_col] + '?format=json'
+        #         # get response, convert to dict
+        #         # response = requests.get(url).json()
+        #         response = requests.get(url,headers=headers).json()
+        #         # print('response:',response)
+        #         # find "sequence" key
+        #         if 'sequence' in response:
+        #             # add sequence to dummy protein
+        #             seq = response['sequence']['value'].replace('*','')
+        #             # convert this excel formula into a method of determining protein mass: =SUMPRODUCT((LEN([@sequence])-LEN(SUBSTITUTE([@sequence],{"A";"C";"D";"E";"F";"G";"H";"I";"K";"L";"M";"N";"P";"Q";"R";"S";"T";"V";"W";"Y"},""))),{72.08;104.14;115.08;129.11;148.17;58.05;138.14;114.16;130.18;114.16;132.2;115.1;98.12;129.13;158.19;88.08;102.1;100.13;187.21;164.17})/1000
+        #             mw = sum([len(seq) - len(seq.replace(aa, '')) for aa in ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']] * np.array([72.08,104.14,115.08,129.11,148.17,58.05,138.14,114.16,130.18,114.16,132.2,115.1,98.12,129.13,158.19,88.08,102.1,100.13,187.21,164.17])) / 1000
+        #             # abundance / wt.
+        #             conc = df_raw.loc[i, cols_data[0]]
+        #             for aa in ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']:
+        #                 # find fraction of amino acid in sequence, multiply by abundance / MW of protein
+        #                 dummy_protein['AA abundances'][aa] += (len(seq) - len(seq.replace(aa, '')) / len(seq)) * conc / mw
+        #             # with open('./nonmodeled_proteins.json', 'a') as f:
+        #             #     f.write(str({'id':i,'URL':url,'sequence':seq,'MW (g/mmol)':mw,'conc (g/gDW)':conc}))
+        #             nonmodel_proteins.append({'id':i,'URL':url,'sequence':seq,'MW (g/mmol)':mw,'conc (g/gDW)':conc})
         else:
             seq = df_prot.loc[i, 'sequence']
             conc = df_raw.loc[i, cols_data[0]]
             mw = df_prot.loc[i, 'MW (g/mmol)']
-        if conc and mw and seq:
-            ATP_cost_of_translation += conc * ((len(seq) * 2) + 1) / mw
+        if conc and mw and seq != '':
+            print(i, conc, mw, seq.replace('*',''))
+            ATP_cost_of_translation += (conc * ptot * ((len(seq.replace('*','')) * 2) + 1) / mw)
 # max_allowed_mito_proteome_allo_fraction = 1 - nonmodeled_proteome_allocation
 # save nonmodeled protein info to JSON
 with open(nonmodel_protein_data_path, 'w') as f:
@@ -354,3 +358,4 @@ if errors:
 print('nonmodeled proteome allocation:', nonmodeled_proteome_allocation)
 # print max allowed mito proteome allocation
 print('max allowed mito proteome allocation:', max_allowed_mito_proteome_allo_fraction)
+print('ATP costs of translation:',ATP_cost_of_translation)
