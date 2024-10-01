b16b7fafe78301fadeb78bb640726f4de461ff9e
Eric Mooney <ejm6426@psu.edu>
2024-10-01 18:47:43 -0400

    yield prediction files from rtRBA

--- application/clear_non_templates.sh
+++ application/clear_non_templates.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #for di in ls -d output_max/!(\(template\)); do rm -rf $di; done
-dir=output_max
+dir=output_max_withoutMP
 cd $dir
 for di in $(ls -d *); do if [[ "$di" != "(template)" ]]; then rm -rf $di; fi; done
