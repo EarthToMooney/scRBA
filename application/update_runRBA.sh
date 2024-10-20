9d15cb5a23d6293a0fd2a7569b40892beb048100
Eric Mooney <ejm6426@psu.edu>
2024-10-20 16:38:12 -0400

    new folder from RT

--- application/update_runRBA.sh
+++ application/update_runRBA.sh
@@ -3,8 +3,11 @@
 #dir=output_max_withoutMP
 #cd $dir
 file='runRBA.py'
+# file='runRBA_GAMS_settings.txt'
 for di in $(ls -d output_max*/*/$file); do
+# for di in $(ls -d output_max_withoutMP_SC_RT_kapp_mapping*/*/$file); do
 	if [[ "$di" != "(template)" && "$di" != "run.sh" ]]; then
 		cp output_max_withoutMP/\(template\)/$file $di
+		# cp output_max_withoutMP_SC_RT_kapp_mapping/\(template\)/$file $di
 	fi
 done
