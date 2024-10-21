31e4909544764418e8c32705671febd4b79382f8
Eric Mooney <ejm6426@psu.edu>
2024-10-21 14:04:28 -0400

    rerun

--- application/output_max_withoutMP_SC_RT_kapp_mapping/sesqt/RBA_defaults_from_FBA.py
+++ application/output_max_withoutMP_SC_RT_kapp_mapping/sesqt/RBA_defaults_from_FBA.py
@@ -6,5 +6,5 @@ biom_id = 'BIOSYN-BIODILAERO'
 biom_id_fba = 'BIOMASS_AERO_SC_hvd'
 vprod_coreid = 'EX_sesqt_e'
 vprod = 'RXNADD-EX_sesqt_e_FWD-SPONT'
-c_sources_list = [{'rxn': 'EX_glc__D_e', 'MW': 180.156, 'v': -5.550726324289716}]
+c_sources_list = [{'rxn': 'EX_glc__D_e', 'MW': 180.156, 'v': -5.550726324289684}]
 prod_mw = 246.3
