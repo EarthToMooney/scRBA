2da2dac4acf9eb79862149b6f27fe283005d6dd6
Eric Mooney <ejm6426@psu.edu>
2024-10-21 10:18:09 -0400

    from roar; rerun

--- application/output_max_withoutMP_SC_RT_kapp_mapping/lac__L/RBA_defaults_from_FBA.py
+++ application/output_max_withoutMP_SC_RT_kapp_mapping/lac__L/RBA_defaults_from_FBA.py
@@ -6,5 +6,5 @@ biom_id = 'BIOSYN-BIODILAERO'
 biom_id_fba = 'BIOMASS_AERO_SC_hvd'
 vprod_coreid = 'EX_lac__L_e'
 vprod = 'RXN-EX_lac__L_e_FWD-SPONT'
-c_sources_list = [{'rxn': 'EX_glc__D_e', 'MW': 180.156, 'v': -5.516810175723823}]
+c_sources_list = [{'rxn': 'EX_glc__D_e', 'MW': 180.156, 'v': -5.516810175723827}]
 prod_mw = 89.07
